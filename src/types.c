b9aecea4bf273ce28f38956b0e00029e7d13b0a5
RagnaR <shaxnurmurodov1990@gmail.com>
2025-12-18 18:47:59 +0500

    учеба, научился переменным

---
+++ src/types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char a = 65, b = 66, c = 67;
+
+    printf("%c", a);
+    printf("%c", b);
+    printf("%c", c);
+    return 0;
+}
+// This program prints the characters corresponding to ASCII values 65, 66, and 67.
